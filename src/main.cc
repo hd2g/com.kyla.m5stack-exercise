6e416f107cf6fb0ae340a39def96ec6cc00b8e23
hd2g <nagisa.159s.8673.trap@gmail.com>
2023-01-11 20:07:47 +0900

    success "Hello, World!"

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,11 @@
-int main(int argc, char* argv[]) {
-  return 0;
+#include <M5Stack.h>
+
+void setup() {
+  M5.begin();
+
+  M5.Lcd.setCursor(20, 60);
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.print("Hello, world!");
 }
+
+void loop() {}
